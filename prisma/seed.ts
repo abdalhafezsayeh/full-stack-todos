fb174ad737c36a2db4d510c205f2d38e26dc1106
Hafez <abdalhafezsayeh1997@gmail.com>
2025-03-01 17:18:39 +0200

    finsh intgration with prisma-mongoDB

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "HelloBro",
+    },
+  });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
