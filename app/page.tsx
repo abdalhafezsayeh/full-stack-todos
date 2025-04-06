887c39a41c18186698425ac921b702a2184bc693
taha.ahmed <abdalhafezsayeh1997@gmail.com>
2025-04-06 18:51:50 +0200

    done

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="w-[95%] mx-auto h-screen">
       {/* Start Navbar  */}
       <div className="flex justify-between items-center border-b mb-2">
-        <h3>Notta</h3>
+        <h3>Notta v2</h3>
         <div className="flex items-center gap-3 justify-end w-full h-[60px]">
           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
